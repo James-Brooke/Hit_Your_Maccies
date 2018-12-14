c33530bfbdf13a7b32f870d68527bf3a0e1aca1c
James-Brooke <jamesbrooke290@gmail.com>
2018-12-13 16:08:03 -0800

    Food model defined

--- website/calculator/models.py
+++ website/calculator/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Food(models.Model):
+    item = models.CharField(max_length=200),
+    cal = models.IntegerField(),
+    fat = models.IntegerField(),
+    sfat = models.IntegerField(),
+    tfat = models.IntegerField(),
+    chol = models.IntegerField(),
+    salt = models.IntegerField(),
+    carb = models.IntegerField(),
+    fbr = models.IntegerField(),
+    sgr = models.IntegerField(),
+    pro = models.IntegerField(),
+    category = models.CharField(max_length=200)
