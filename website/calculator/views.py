68ce44950786c3827ab5731dd34a09e31aad75e1
James-Brooke <jamesbrooke290@gmail.com>
2018-12-14 19:54:40 -0800

    Basic table template working

--- website/calculator/views.py
+++ website/calculator/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Food
 
 def index(request):
-    foods = Food.objects.order_by('cal')[:5]
-    output = ', '.join([food.item for food in foods])
-    return HttpResponse(output)
+    foods = Food.objects.order_by('-cal')[:5]
+    context = {'foods': foods,}
+    return render(request, 'calculator/index.html', context)
 
