ec5902a445abf357f238dd6d7fa2370762dde09e
James-Brooke <jamesbrooke290@gmail.com>
2018-12-16 15:34:08 -0800

    Added dropdown for menu type

--- website/calculator/forms.py
+++ website/calculator/forms.py
@@ -1,11 +1,21 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import ModelForm, Select
 from .models import Food
 
 class CalcuFilter(ModelForm):
 
+    CATEGORIES = (
+        ('ALL', 'ANY'),
+        ('CHI', 'CHICKEN'),
+        ('BUR', 'BURGER')
+    )
+
+    category = forms.ChoiceField(choices=CATEGORIES, required=False)
+
+
     class Meta:
         model = Food
-        fields = ['pro']
+        fields = ['pro', 'category']
         labels = {
             'pro': 'How many grams of protein do you need?   ',
         }
