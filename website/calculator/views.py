a18d4c5ecbc8c72c50fe973f46c23e2da0fb9010
James-Brooke <jamesbrooke290@gmail.com>
2018-12-15 20:32:52 -0800

    Added input box for calories

--- website/calculator/views.py
+++ website/calculator/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Food
+from .forms import CalcuFilter
 
 def index(request):
     foods = Food.objects.order_by('-cal')[:5]
-    context = {'foods': foods,}
+
+    if request.method == 'POST':
+        form = CalcuFilter(request.POST)
+        if form.is_valid():
+            pass #trigger validation
+    else:
+        form = CalcuFilter()
+
+    context = {'foods': foods,
+                'form': form}
     return render(request, 'calculator/index.html', context)
 
+
+
+
+
+
+def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass  # does nothing, just trigger the validation
+    else:
+        form = ContactForm()
+    return render(request, 'home.html', {'form': form})
